edd97c39ceaf332e0aa54e755c345cbc60d2e116
DikyFahrezy <dikyfahrezy@gmail.com>
2022-09-14 02:54:14 +0700

    mengupload file Tombol

--- Tombol/Tombol.ino
+++ Tombol/Tombol.ino
@@ -27,25 +27,25 @@ void loop()
 if ( Led == 1)
  {
   digitalWrite(11,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(11,LOW);
   digitalWrite(12,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(12,LOW);
   digitalWrite(13,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(13,LOW);
   }
 if ( Led == 2)
  {
   digitalWrite(13,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(13,LOW);
   digitalWrite(12,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(12,LOW);
   digitalWrite(11,HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(11,LOW);
  }
 if ( Led == 0)
